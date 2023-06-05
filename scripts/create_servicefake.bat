f82d672561e3ddc13c46b09c3186981a31becd0b
Elyor Khojikhonov <e.hadjiahnov@gmail.com>
2023-06-05 23:24:35 +0500

    move create service logic to before install

--- scripts/create_servicefake.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-nssm remove LibraryWebApp confirm
-nssm install LibraryWebApp "C:\inetpub\wwwroot\LibraryWebApp\published\LibraryWebApp.exe"
-nssm set LibraryWebApp Start SERVICE_AUTO_START
-nssm set LibraryWebApp AppStdout "C:\inetpub\wwwroot\LibraryWebApp\logs\stdout.log"
-nssm set LibraryWebApp AppStderr "C:\inetpub\wwwroot\LibraryWebApp\logs\stderr.log"
-nssm set LibraryWebApp AppEnvironmentExtra ASPNETCORE_URLS=http://*:80
-nssm start LibraryWebApp
