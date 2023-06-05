0cd0f1fe13508c63a0ed5ee3ed04c2c4285b0915
Elyor Khojikhonov <e.hadjiahnov@gmail.com>
2023-06-05 16:51:53 +0500

    update scripts

--- scripts/create_service.bat
+++ scripts/create_service.bat
@@ -3,4 +3,3 @@
 nssm remove LibraryWebApp confirm
 nssm install LibraryWebApp "C:\inetpub\wwwroot\LibraryWebApp\published\LibraryWebApp.exe"
 nssm set LibraryWebApp Start SERVICE_AUTO_START
-nssm start LibraryWebApp
