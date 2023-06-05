04a74ccce820dd201290b03bc06d919249f4df36
Elyor Khojikhonov <e.hadjiahnov@gmail.com>
2023-06-05 23:30:32 +0500

    try fix scripts

---
+++ scripts/before_install.bat
@@ -0,0 +1,3 @@
+@echo off
+rm -rf C:\inetpub\wwwroot\LibraryWebApp
+nssm remove LibraryWebApp confirm
--- scripts/install.bat
+++ scripts/install.bat
@@ -1,9 +1,6 @@
 @echo off
-rm -rf C:\inetpub\wwwroot\LibraryWebApp
 cd C:\inetpub\wwwroot\LibraryWebApp
 dotnet publish -c Release -o ./published
-
-nssm remove LibraryWebApp confirm
 nssm install LibraryWebApp "C:\inetpub\wwwroot\LibraryWebApp\published\LibraryWebApp.exe"
 nssm set LibraryWebApp Start SERVICE_AUTO_START
 nssm set LibraryWebApp AppStdout "C:\inetpub\wwwroot\LibraryWebApp\logs\stdout.log"
