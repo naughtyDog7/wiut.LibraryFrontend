5e4d8adb7eb0af99e0710b4f31e77dcdaa2bcdad
Elyor Khojikhonov <e.hadjiahnov@gmail.com>
2023-06-05 15:55:40 +0500

    update before script

--- scripts/before_install.bat
+++ scripts/before_install.bat
@@ -1,6 +1,4 @@
 @echo off
-
-dotnet --version || powershell -Command "& { iwr -outf install-dotnet.ps1 https://dot.net/v1/dotnet-install.ps1; ./install-dotnet.ps1 -Version 6.0 }"
 cd C:\inetpub\wwwroot\LibraryWebApp
 dotnet publish -c Release -o ./published
 call scripts\create_service.bat
